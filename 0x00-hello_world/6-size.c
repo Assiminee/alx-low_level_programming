405dd5d3b0cc7c8928ed8d4ddbc77a523ff512b5
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-17 07:44:40 -0700

    First C Programming Project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("size of char: %d bytes\n", sizeof(char));
+	printf("size of int: %d bytes\n", sizeof(int));
+	printf("size of long int: %d bytes\n", sizeof(long int));
+	printf("size of long long: %d bytes\n", sizeof(long long));
+	printf("size of float: %d bytes\n", sizeof(float));
+}
