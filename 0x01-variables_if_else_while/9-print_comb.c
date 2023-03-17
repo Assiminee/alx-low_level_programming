ef4457a4e05bd24d017aad4c1e6c07313f975839
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-17 12:23:16 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j = 10;
+	int x = 44;
+	int y = 32;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(y);
+		putchar(x);
+	}
+
+	putchar(j);
+
+	return (0);
+}
