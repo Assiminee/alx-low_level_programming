95ef3a98879785b986c1e2143683b56881e2bdbd
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-18 05:18:14 -0700

    First C Programming Project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of char: %lu bytes\n", sizeof(char));
-	printf("size of int: %lu bytes\n", sizeof(int));
-	printf("size of long int: %lu bytes\n", sizeof(long int));
-	printf("size of long long: %lu bytes\n", sizeof(long long));
-	printf("size of float: %lu bytes\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
