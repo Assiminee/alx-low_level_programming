263bef1e8e00cc8253323dfb167b14957e75a8ba
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-23 10:54:42 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _isupper(int c)
+{
+    if ('A' <= c && 'Z' >= c)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
+
+#endif
