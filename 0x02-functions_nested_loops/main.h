007a19baf15f1c06c3914db4f1e1237500a47224
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-22 17:26:22 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void _putchar(void);
+/**
+ * void function _putchar
+ *
+ * 
+ *
+ */
+void _putchar(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
