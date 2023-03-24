0724cd642daabd95da005afadae7496c47324e49
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-23 18:15:04 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle.
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = size; i > 0; i--)
+	{
+		for (k = i - 1; k > 0; k--)
+		{
+			_putchar(' ');
+		}
+		for (j = i; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
