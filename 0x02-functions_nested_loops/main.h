5839acfae27a70026889c82f9b76934ac19014b6
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-24 18:45:28 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,15 +4,27 @@
  * _putchar - writes the character c
  * to stdout
  * @c: parameter to compare
+ *
  * Return: 1 or 0
  */
 int _putchar(char c);
 /**
  * print_alphabet - prints the alphabet
+ *
+ * return: void
  */
 void print_alphabet(void);
 /**
  * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * return: void
  */
 void print_alphabet_x10(void);
+/**
+ * _islower - checks if character is lowercase
+ * @c: character to be checked
+ *
+ * return: 1 or 0
+ */
+int _islower(int c);
 #endif
