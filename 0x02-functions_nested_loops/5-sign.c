4884fe777d91559fdd0fa572599a0a1b19607433
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-25 10:37:17 +0000

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print sign - prints sign based on input
+ * print_sign - prints sign based on input
  * @n: number to check
  *
  * Return: 1, 0, or -1
@@ -12,7 +12,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
