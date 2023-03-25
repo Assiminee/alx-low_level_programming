fd2252b19812aec3bae770c6c4dcc731b128c140
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-25 17:04:52 +0000

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,17 +13,20 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			if (i * j > 9)
 			{
 				_putchar((i * j) / 10 + '0');
 			}
-			_putchar((i * j) % 10 + '0');
-			if (j != 9)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
+			_putchar((i * j) % 10 + '0');
 		}
 		_putchar('\n');
 	}
