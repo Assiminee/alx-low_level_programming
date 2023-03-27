091fd72326a0f4bb76eae90cc2cdd8a0e69db4a1
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-27 23:38:55 +0000

    pointers arrays strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,14 @@
  * _strlen - return length of string
  * @s: pointer to char
  *
- * return: int
+ * return: length of string
  */
 int _strlen(char *s)
 {
 	int len;
 
 	len = 0;
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		len++;
 		s++;
