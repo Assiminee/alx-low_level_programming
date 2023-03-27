174a7f165a4097041ea6390a733c0c1472b438ac
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-27 23:23:24 +0000

    pointers arrays strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,9 +9,17 @@
 int _putchar(char c);
 /**
  * reset_to_98 - update the value *n is pointing to
- * @*n: pointer to integer
+ * @n: pointer to integer
  *
  * retun: void
  */
 void reset_to_98(int *n);
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to integer
+ * @b: pointer to integer
+ *
+ * return: void
+ */
+void swap_int(int *a, int *b);
 #endif
