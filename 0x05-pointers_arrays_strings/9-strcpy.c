358623e3538e627dbe6924cbcee7e73cacc1d0f3
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-03-28 01:57:20 +0000

    pointers arrays strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,16 +10,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	unsigned long int i;
+	int i;
 
-	dest = malloc(strlen(src));
-	if (dest == NULL)
+	i = 0;
+
+	while (src[i] != '\0')
 	{
-		return NULL;
-	}
-
-	for (i = 0; i < strlen(src); i++)
 		dest[i] = src[i];
+		i++;
+	}
 
 	dest[i] = '\0'; 
 	return (dest);
