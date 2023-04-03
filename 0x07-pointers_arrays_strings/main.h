6b05b974c074440a4af26f262f2ef2efdc024641
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-03 16:09:04 -0700

    pointers arrays strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: constant byte to fill the memory area
+ * @n: the first n bytes of memory to be filled
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n);
+#endif
