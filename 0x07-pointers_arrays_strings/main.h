10187c643c3e9e74b01af159f61fe25c8f939b8e
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-03 16:22:13 -0700

    pointers arrays strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,13 @@
  * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n);
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: memory area to to which n bytes of src will be copied
+ * @src: memory area from which n bytes will be copied
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
