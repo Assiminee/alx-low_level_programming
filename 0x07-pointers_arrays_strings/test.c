b1091618fa2843a928aaa0cf46060816b38aebfe
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-04 09:44:28 -0700

    pointers arrays strings

--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -12,14 +12,25 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sum1;
 	int sum2;
-	
-	sum1 = 0;
+	int *ptr1;
+	int *ptr2;
+
+	ptr1 = a;
+	ptr2 = a;
+	sum1 = *ptr1;
 	sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		sum1 += *(a + i*size + i);
-		sum2 += *(a + i*size + size - i - 1);
+		ptr2++;
 	}
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++, ptr1 += size + 1)
+	{
+		sum1 += *ptr1;
+	}
+	for (i = 0; i < size; i++, ptr2 += (size - 1))
+	{
+		sum2 += *ptr2;
+	}
+	printf("%d, %d\ n", sum1, sum2);
 }
