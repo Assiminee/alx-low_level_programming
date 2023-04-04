61a341694058fe58b04364aafabf364f029327a5
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-03 19:08:57 -0700

    pointers arrays strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -65,4 +65,12 @@ void print_chessboard(char (*a)[8]);
  * Return: 1 or 0
  */
 int _putchar(char c);
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer to char
+ * @to: pointer to char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to);
 #endif
