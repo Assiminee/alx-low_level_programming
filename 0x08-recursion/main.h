3c8468d0fd15c55d8e9c3a3cba54b2e062e5e1e8
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-05 17:02:18 +0000

    pointers arrays strings

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+/**
+ * _putchar - writes the character c
+ * to stdout
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s);
+#endif
