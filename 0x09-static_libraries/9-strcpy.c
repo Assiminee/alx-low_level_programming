c63977a6693e162126e56dee423b526cc0402e12
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-09 01:03:51 +0000

    C - Static libraries

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strcpy - copies src to dest including null terminating char
+ * @dest: pointer to char. destination string.
+ * @src: pointer to char. src string.
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
