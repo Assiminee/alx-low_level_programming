69e3df5d7975cc1ecac6235382c63228ed9405ce
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-09 01:26:16 +0000

    C - Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 /**
- * _putchar - writes the character c
- * to stdout
- * @c: parameter to compare
+ * _putchar - writes the character c to stdout
+ * @c: char
  *
  * Return: 1 or 0
  */
