f9bff0aaa1434d9c33ed109d42a17b4c562fc33b
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-10 00:15:15 +0000

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,25 +6,27 @@
  * @argc: number of arguments in the command line
  * @argv: vector of arguments
  *
- * Return: 0 if argc > 1, 1 if argc == 1 
+ * Return: Always 0 
  */
 int main(int __attribute__((unused)) argc, char **argv)
 {
-	long int result;
-	int i;
+	int n1;
+	int n2;
 
-	result = 1;
-	if (argc == 1)
+	n1 = 0;
+	n2 = 0;
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d", n1 * n2);
+		printf("\n");
+	}
+	else
 	{
 		printf("Error");
 		printf("\n");
 		return (1);
 	}
-	for (i = 1; i < argc; i++)
-	{
-		result *= atoi(argv[i]);
-	}
-	printf("%ld", result);
-	printf("\n");
-	exit (EXIT_SUCCESS);
+	return (0);
 }
