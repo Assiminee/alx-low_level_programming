bb46c38278f11a97afd736839a5dc742d55e0a79
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-10 22:49:08 +0000

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,7 @@
  * @argc: number of arguments in the command line
  * @argv: vector of arguments
  *
- * Return: Always 0
+ * Return: 0 or 1
  */
 int main(int argc, char** argv)
 {
@@ -18,20 +18,19 @@ int main(int argc, char** argv)
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	coins += cents/25;
+	coins += cents / 25;
 	cents %= 25;
-	coins += cents/10;
+	coins += cents / 10;
 	cents %= 10;
-	coins += cents/5;
+	coins += cents / 5;
 	cents %= 5;
-	coins += cents/2;
+	coins += cents / 2;
 	cents %= 2;
 	coins += cents;
 	printf("%d\n", coins);
