16bb11aca7691f2b2ffad3b7837ccd11dad3524d
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-13 23:30:50 +0000

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,8 +15,14 @@ int *array_range(int min, int max)
 	int j;
 
 	if (min > max)
+	{
 		return (NULL);
+	}
 	ptr = malloc((max - min + 1) * sizeof(int));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	i = 0;
 	j = min;
 	while (j <= max)
