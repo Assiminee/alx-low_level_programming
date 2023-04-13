d86ca560e8659db77169dcccdcfa89554560a405
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-13 21:51:23 +0000

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+char *copy_str(char *s1, unsigned int n);
 /*
  * string_nconcat - concatenates two strings.
  * @s1: string to be concatenated
@@ -17,6 +18,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 	int j;
 	unsigned int mem_al;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		ptr = malloc(1);
+		ptr[0] = '\0';
+		return (ptr);
+	}
+	if (s1 == NULL)
+	{
+		if (n >= strlen(s2))
+		{
+			ptr = copy_str(s2, strlen(s2));
+		}
+		else
+		{
+			ptr = copy_str(s2, n);
+		}
+		return (ptr);
+	}
+	if (s2 == NULL)
+        {
+	       	ptr = copy_str(s1, strlen(s1));
+		return (ptr);
+        }
 
 	if (n >= strlen(s2))
 		mem_al = strlen(s1) + strlen(s2) + 1;
@@ -45,4 +70,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 	}
 	return (ptr);
+}
+/*
+ * copy_str - copies string
+ * @s1: string to be copied
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the copied string
+ */
+char *copy_str(char *s1, unsigned int n)
+{
+	unsigned int i;
+	char *ptr;
+
+	ptr = malloc(n + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < n; i++)
+		ptr[i] = s1[i];
+	ptr[i] = '\0';
+
+        return (ptr);
 }
