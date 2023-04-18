907cc598879f696362ff0bc91fbb38cf2081b5ac
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-18 00:51:07 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stddef.h>
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: a dog object
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t newDog;
+	dog_t *ptr = &newDog;
+
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
