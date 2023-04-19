094bec584dda37a7132a82e172cfd7344d6f846a
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-19 22:37:34 +0000

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != 0 && f != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+
+		}
+	}
 }
