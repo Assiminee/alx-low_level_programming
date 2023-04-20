32084b15bec81889610ba9afe2411b98e2c8adf6
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-20 20:25:21 +0000

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,6 +9,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int s;
+
 	va_list args;
 	va_start(args, n);
 
@@ -17,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	s = 0;
 	for (i = 0; i < n; i++)
 	{
-		int x = va_arg(args, int);
+		int x = va_arg (args, int);
 		s += x;
 	}
 	va_end(args);
