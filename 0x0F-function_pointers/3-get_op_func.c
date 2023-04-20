9ff97006dd31944b449b0188a9156cf005786dce
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-20 00:26:13 +0000

    0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stddef.h>
 /**
  * get_op_func - selects the correct function
  * to perform the operation asked by the user
@@ -22,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (ops[i].op == *s)
+		if (*(ops[i].op) == *s)
 		{
 			operation = ops[i].f;
 			return (operation);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,21 +8,26 @@
  *
  * Return: always 0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	int num1, num2;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(argv[2])(num1, num2) == NULL)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	if (*argv[2] == '/' && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
 	}
 	printf("%d\n", get_op_func(argv[2])(num1, num2));
 	return (0);
