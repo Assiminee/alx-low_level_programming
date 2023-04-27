e4d7b4a289506ee7a3e5f94cdfa9b637ba6c07ea
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-27 20:29:54 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 /**
+ * add_node - adds a node to the beginning of a linked list
+ * @head: head of list
+ * @str: string to be added
+ *
+ * Return: linked list
  */
 list_t *add_node(list_t **head, const char *str)
 {
