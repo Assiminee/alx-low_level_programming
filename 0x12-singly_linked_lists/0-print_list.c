55bc2623afa8984dcf4331ddfa332c2a86426c08
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-27 20:07:27 +0000

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,21 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements of a linked list
- * @h: the head of the linked list
+ * print_list - Prints all elements of a linked list
+ * @h: A linked list
  *
- * Return: the number of nodes
+ * Return: The number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		count++;
 		h = h->next;
 	}
