315343053218c4d044e839a2b3dbe3a212ff92dc
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-27 20:27:53 +0000

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
