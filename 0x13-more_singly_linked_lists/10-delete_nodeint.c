ccef21dd0063fa9cdc34f08bacdf176293a27b47
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-05-02 20:04:14 +0100

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+int pop_listint(listint_t **head);
+/**
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *prev;
+	unsigned int i = 0;
+
+	if (index == 0 || *head == NULL)
+		return (-1);
+	if (index == 1)
+	{
+		pop_listint(head);
+		return (1);
+	}
+	while (current != NULL)
+	{
+		if (i == index - 1)
+		{
+			prev = current;
+		}
+		if (i == index)
+		{
+			prev->next = current->next;
+			free(current);
+			return (1);
+		}
+		i++;
+		current = current->next;
+	}
+	return (-1);
+}
+/**
+ * pop_listint - deletes the first node of a list
+ * @head: head of list
+ *
+ * Return: the deleted node's data
+ */
+int pop_listint(listint_t **head)
+{
+        listint_t *tmp = *head;
+        int num = 0;
+
+        if (*head == NULL || head == NULL)
+                return (num);
+        num = tmp->n;
+        *head = (*head)->next;
+        free(tmp);
+	return (num);
+}
