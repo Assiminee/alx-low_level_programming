7ea0e59b65195c697c5192c1f48cee7169b2c177
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-05-02 20:08:38 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 int pop_listint(listint_t **head);
 /**
+ * delete_nodeint_at_index - deletes a node at a given index
+ * @head: head of list
+ * @index: index of node to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
