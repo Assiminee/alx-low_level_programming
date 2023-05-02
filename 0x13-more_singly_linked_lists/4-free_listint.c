b2aba11c66f71e6b1847aab29a24251607397334
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-05-02 12:55:10 +0100

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: the head of the list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *tmp;
+	while (current != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+}
