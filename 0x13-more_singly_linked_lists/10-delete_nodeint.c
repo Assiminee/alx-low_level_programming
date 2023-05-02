4eb5173b1a4963da0431753b40e6d8b88c295833
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-05-02 20:06:51 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -40,13 +40,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
  */
 int pop_listint(listint_t **head)
 {
-        listint_t *tmp = *head;
-        int num = 0;
+	listint_t *tmp = *head;
+	int num = 0;
 
-        if (*head == NULL || head == NULL)
-                return (num);
-        num = tmp->n;
-        *head = (*head)->next;
-        free(tmp);
+	if (*head == NULL || head == NULL)
+		return (num);
+	num = tmp->n;
+	*head = (*head)->next;
+	free(tmp);
 	return (num);
 }
