8d0207bd4654c7faafc97769665225e682426cae
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-05-02 19:08:08 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
 /**
  * insert_nodeint_at_index - inserts a node at a given index
  * @head: head of list
@@ -85,36 +84,4 @@ listint_t *add_nodeint(listint_t **head, const int n)
         new->next = *head;
         *head = new;
         return (*head);
-}
-/**
- * add_nodeint_end - adds a node to the end of the list
- * @head: the head of the list
- * @n: the number to be added
- *
- * Return: the adress of the node added
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-        listint_t *new = malloc(sizeof(listint_t));
-        listint_t *ptr;
-        int num = n;
-
-        if (new == NULL)
-                return (NULL);
-        new->n = num;
-        new->next = NULL;
-        if (*head == NULL)
-        {
-                *head = new;
-        }
-        else
-        {
-                ptr = *head;
-                while (ptr->next != NULL)
-                {
-                        ptr = ptr->next;
-                }
-                ptr->next = new;
-        }
-        return (new);
 }
