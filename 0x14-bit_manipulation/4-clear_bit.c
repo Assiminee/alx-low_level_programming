d547a519fbb172890dec7445468e01302de8a743
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-05-06 01:11:51 +0100

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned long int bit = 1UL << index;
+        unsigned long int bit = 1UL << (index + 1);
 
         if (n == NULL)
                 return (-1);
-	if (*n != 0)
-        	*n = *n - bit;
+        *n = *n & bit;
         return (1);
 }
