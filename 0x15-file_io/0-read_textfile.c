b89638b83f1a67413bbee1a2056164d2e5ecb278
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-07-12 17:50:27 +0100

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,28 +8,44 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buff[1024];
-	int fd = open(filename, O_RDONLY);
-	ssize_t charsWritten = 0;
-	ssize_t charsRead;
-	ssize_t count;
+	int fd;
+	ssize_t charsRead, count, charsWritten = 0;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
-	if (fd == -1)
+	if ((fd = open(filename, O_RDONLY)) == -1)
 		return (0);
+	buff = malloc(letters + 1);
+	if (buff == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
 	charsRead = read(fd, buff, letters);
 	if (charsRead == -1)
+	{
+		close(fd);
+		free(buff);
 		return (0);
+	}
 	while (charsWritten < charsRead)
 	{
 		count = write(STDOUT_FILENO, buff + charsWritten, charsRead - charsWritten);
 		if (count == -1)
+		{
+			close(fd);
+			free(buff);
 			return (0);
+		}
 		charsWritten += count;
 	}
+	close(fd);
+	free(buff);
 	if (charsWritten != charsRead)
+	{
 		return (0);
+	}
 	return (charsWritten);
 }
