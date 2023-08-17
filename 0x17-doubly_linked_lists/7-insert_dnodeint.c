eacc680ebe00289dfd4485347741eb9ee7026dc2
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 21:57:10 +0100

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,32 +24,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (i == idx - 1)
 	{
+		/*
 		if ((*h)->next == NULL)
 		{
 			while ((*h)->prev != NULL)
                         	*h = (*h)->prev;
 			return (add_dnodeint_end(h, n));
 		}
-		else
-		{
-			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
-				return (NULL);
+		*/
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
 
-			new_node->n = n;
-			new_node->next = NULL;
-			new_node->prev = NULL;
+		new_node->n = n;
+		new_node->next = NULL;
+		new_node->prev = NULL;
 
-			tmp = (*h)->next;
-			tmp->prev = new_node;
-			(*h)->next = new_node;
-			new_node->next = tmp;
-			new_node->prev = *h;
-			*h = new_node;
-			while ((*h)->prev != NULL)
-				*h = (*h)->prev;
-			return (new_node);
-		}
+		tmp = (*h)->next;
+		tmp->prev = new_node;
+		(*h)->next = new_node;
+		new_node->next = tmp;
+		new_node->prev = *h;
+		*h = new_node;
+		while ((*h)->prev != NULL)
+			*h = (*h)->prev;
+		return (new_node);
 	}
 	return (NULL);
 }
