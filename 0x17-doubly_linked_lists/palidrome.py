09679d72cab19947934d3846edd5c14a9dbee73b
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 23:28:30 +0100

    req

--- 0x17-doubly_linked_lists/palidrome.py
+++ 0x17-doubly_linked_lists/palidrome.py
@@ -6,4 +6,4 @@ for num1 in range(100, 1000):
         prod = num1 * num2
         if str(prod) == str(prod)[::-1]:
             palindromes.append(prod)
-print(max(palindromes))
+print("{:d}".format(max(palindromes)), end="")
