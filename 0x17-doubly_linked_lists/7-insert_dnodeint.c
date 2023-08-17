8db22652036a29c12d7a9c904972c32fe45ca7e7
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 22:22:19 +0100

    Finally

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,9 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	curr = *h;
-
 	while (curr != NULL && i < idx - 1)
 	{
 		curr = curr->next;
@@ -29,18 +27,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (curr->next == NULL)
 			return (add_dnodeint_end(h, n));
-		else
-		{
-			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
-				return (NULL);
-			new_node->n = n;
-			new_node->next = curr->next;
-			new_node->prev = curr;
-			curr->next->prev = new_node;
-			curr->next = new_node;
-			return (new_node);
-		}
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = curr->next;
+		new_node->prev = curr;
+		curr->next->prev = new_node;
+		curr->next = new_node;
+		return (new_node);
 	}
 	return (NULL);
 }
