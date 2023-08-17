ccabcb23a8fc4e136f4ebf7cc397fce0c4138349
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 22:10:30 +0100

    test2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	if (idx == 0 && (*h) == NULL)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
 	while ((*h) != NULL && i < idx - 1)
 	{
