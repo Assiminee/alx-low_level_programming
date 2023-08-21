8c696a6bd0da31d9a12f5a23bca47214686df751
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-21 18:46:56 +0100

    task0

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - finds the first occurrence of substring needle in string haystack
+ * @haystack: string to be searched
+ * @needle: string to be searched for
+ *
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *hptr;
+	char *nptr;
+
+	while (*haystack)
+	{
+		hptr = haystack;
+		nptr = needle;
+		while (*nptr && *nptr == *hptr)
+		{
+			hptr++;
+			nptr++;
+		}
+		if (*nptr == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
