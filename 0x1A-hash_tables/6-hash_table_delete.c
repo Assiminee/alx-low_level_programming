749930c5e7bea6e288525f4b639385b09525e210
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-09 00:37:42 +0100

    test

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr, *tmp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr)
+		{
+			tmp = curr->next;
+			free(curr->key);
+			free(curr->value);
+			free(curr);
+			curr = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
