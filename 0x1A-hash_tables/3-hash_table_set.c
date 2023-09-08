f3e3de2e451902ecbe3cbee1872b7cb06c6919c0
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-08 23:58:07 +0100

    tweaks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (!ht || !key || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -25,11 +25,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node = malloc(sizeof(hash_node_t));
 
-	if (new_node == NULL)
+	if (!new_node)
 		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
