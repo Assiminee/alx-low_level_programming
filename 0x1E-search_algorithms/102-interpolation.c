e33df3b2d48fc4d129c4a22d0a20f498593a80d8
Assiminee <znatni.yasmine@gmail.com>
2024-07-16 19:07:02 +0100

    msg

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -44,7 +44,10 @@ int interpolation_search(int *array, size_t size, int value)
 		if (array[pos] > value)
 			high = pos - 1;
 		else
-			low = pos;
+			low = pos + 1;
+
+		if (low == high)
+			return (-1);
 	}
 	printf("Value checked array[%ld] is out of range\n", pos);
 	return -1;
