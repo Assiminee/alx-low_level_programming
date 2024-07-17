1222fe2313f6502678e1bebe2397c6eea1fe40c9
Assiminee <znatni.yasmine@gmail.com>
2024-07-17 13:53:14 +0100

    msg

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,17 +18,17 @@ int interpolation_search(int *array, size_t size, int value)
 
 	low = 0;
 	high = size - 1;
+	pos = 0;
 
-	while (
-			low < high &&
-			value >= array[low] && value <= array[high]
-			)
+	while (low < high)
 	{
 		pos = low + (
 				((double)(high - low) / (array[high] - array[low])) *
 				(value - array[low])
 				);
 
+		if (pos >= size)
+			break;
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] < value)
